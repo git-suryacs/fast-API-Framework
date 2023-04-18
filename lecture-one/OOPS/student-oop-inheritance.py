0b248f46bbd66ad0df9713213638890d045fa552
git-suryacs <2suryacs@gmail.com>
2023-04-18 13:07:27 +0100

    Conclusion of inheritance in py

--- lecture-one/OOPS/student-oop-inheritance.py
+++ lecture-one/OOPS/student-oop-inheritance.py
@@ -4,11 +4,29 @@ class Student:
         self.last_name = last_name
 
     def greetings(self):
-        return f'Hello! I am {self.first_name} {self.last_name}'
+        return f'Hello! I am {self.first_name} {self.last_name} from parent class'
 
-student_1 = Student('Surya','CS')
+
+class CollegeStudent(Student):
+    def __init__(self,first_name,last_name,major):
+        super().__init__(first_name,last_name)
+        self.major = major
+
+    def greetings(self):
+        return f'{self.first_name} is a college student'
+
+class NonCollegeStudent(Student):
+    def __init__(self,first_name,last_name,future_job):
+        super().__init__(first_name,last_name)
+        self.future_job =future_job
+
+student_1 = CollegeStudent('Surya','CS','Computer Science')
 student_2 = Student('John','Miller')
+student_3 = NonCollegeStudent('Rohit','Miller','Pilot')
+
 
 
 print(student_1.greetings())
 print(student_2.greetings())
+print(student_1.major)
+print(student_3.future_job)
