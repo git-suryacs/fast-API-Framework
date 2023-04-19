3f0bc0b54ee92edd17fff98b3a630eba8146c7a9
git-suryacs <2suryacs@gmail.com>
2023-04-19 15:40:36 +0100

    Static path params vs Dynamic path params

--- books.py
+++ books.py
@@ -1,17 +1,31 @@
+from typing import Dict
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
 BOOKS = [
-    {'title':'Title One','author':'Author One','category':'science'},
-    {'title':'Title Two','author':'Author Two','category':'science'},
-    {'title':'Title Three','author':'Author Three','category':'history'},
-    {'title':'Title Four','author':'Author Four','category':'math'},
-    {'title':'Title Five','author':'Author Five','category':'history'},
-    {'title':'Title Six','author':'Author Two','category':'math'}
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'history'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
 ]
+
 
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+
+# @app.get("/books/mybook")
+# async def read_all_books():
+#     return {'book_title':'My fav Book!'}
+
+
+@app.get("/books/{book_title}")
+async def read_book(book_title: str) -> dict:
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
