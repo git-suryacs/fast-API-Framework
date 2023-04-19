d6bc5bb3db879ef6ce54c7a4fe98016cdcbda363
git-suryacs <2suryacs@gmail.com>
2023-04-19 17:36:21 +0100

    Combining both Query param and path param

--- books.py
+++ books.py
@@ -1,5 +1,3 @@
-from typing import Dict
-
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -32,10 +30,20 @@ async def read_book(book_title: str) -> dict:
 
 
 @app.get("/books/")
-async def read_category_by_query(category : str):
+async def read_category_by_query(category: str):
     books_to_return = []
     for book in BOOKS:
         if book.get('category').casefold() == category.casefold():
             books_to_return.append(book)
     return books_to_return
 
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() \
+                and book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
