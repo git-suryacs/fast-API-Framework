48de6af2cc06962bff74d0c702515d92ab56fe71
git-suryacs <2suryacs@gmail.com>
2023-04-21 18:29:41 +0100

    Assignment completion

--- books2.py
+++ books2.py
@@ -12,13 +12,15 @@ class Book:
     author: str
     description: str
     rating: int
+    published_date: int
 
-    def __init__(self, id, title, author, description, rating):
+    def __init__(self, id, title, author, description, rating, published_date):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 
 class BookRequest(BaseModel):
@@ -27,25 +29,27 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=99)
     rating: int = Field(gt=-1, lt=6)
+    published_date: int = Field(gt=1999,lt=2023)
 
     class Config:
         schema_extra = {
-            'example':{
+            'example': {
                 'title': 'A new book',
                 'author': 'surya',
                 'description': 'A new description of a book',
-                'rating' : 5
+                'rating': 5,
+                'published_date':2019
             }
         }
 
 
 BOOKS = [
-    Book(1, 'Computer Science Pro', 'surya', 'A very nice book', 5),
-    Book(2, 'Be fast with FastAPI', 'surya', 'This is a great book', 5),
-    Book(3, 'Master Endpoints', 'surya', 'An awesome book', 5),
-    Book(4, 'HP1', 'author1', 'Book desc', 2),
-    Book(5, 'HP2', 'author2', 'Book desc', 3),
-    Book(6, 'HP3', 'author3', 'Book desc', 1)
+    Book(1, 'Computer Science Pro', 'surya', 'A very nice book', 5, 1999),
+    Book(2, 'Be fast with FastAPI', 'surya', 'This is a great book', 5, 2005),
+    Book(3, 'Master Endpoints', 'surya', 'An awesome book', 5, 2009),
+    Book(4, 'HP1', 'author1', 'Book desc', 2, 2007),
+    Book(5, 'HP2', 'author2', 'Book desc', 3, 2010),
+    Book(6, 'HP3', 'author3', 'Book desc', 1, 2022)
 ]
 
 
@@ -54,21 +58,31 @@ async def read_all_books():
     return BOOKS
 
 
+
 @app.get("/books/{book_id}")
-async def read_book_by_id(book_id:int):
+async def read_book_by_id(book_id: int):
     for book in BOOKS:
         if book.id == book_id:
             return book
 
 
+
 @app.get("/books/")
-async def read_books_by_rating(book_rating :int):
+async def read_books_by_rating(book_rating: int):
     list_of_books = []
     for book in BOOKS:
-        if book.rating == book_rating :
+        if book.rating == book_rating:
             list_of_books.append(book)
     return list_of_books
 
+
+@app.get("/books/publish/{year}")
+async def read_books_by_published_year(year:int):
+    list_of_books = []
+    for book in BOOKS:
+        if book.published_date == year:
+            list_of_books.append(book)
+    return list_of_books
 
 @app.post("/create-book")
 async def create_book(book_request: BookRequest):
@@ -77,10 +91,18 @@ async def create_book(book_request: BookRequest):
 
 
 @app.put("/books/update_book")
-async def update_book(book:BookRequest):
+async def update_book(book: BookRequest):
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book.id:
             BOOKS[i] = book
+
+
+@app.delete("/books/{book_id}")
+async def delete_book_by_id(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            BOOKS.remove(book)
+            break
 
 
 def find_book_id(book: Book):
