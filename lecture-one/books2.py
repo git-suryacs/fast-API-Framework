e96018da19621010d87abe5be9c8aee485ddb7d0
git-suryacs <2suryacs@gmail.com>
2023-04-22 14:35:06 +0100

    Database connection with ORM SQLALCHEMY

--- lecture-one/books2.py
+++ lecture-one/books2.py
@@ -1,7 +1,8 @@
 from typing import Optional
 
-from fastapi import FastAPI,Path
+from fastapi import FastAPI, Path, Query, HTTPException
 from pydantic import BaseModel, Field
+from starlette import status
 
 app = FastAPI()
 
@@ -29,7 +30,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=99)
     rating: int = Field(gt=-1, lt=6)
-    published_date: int = Field(gt=1999,lt=2023)
+    published_date: int = Field(gt=1999, lt=2023)
 
     class Config:
         schema_extra = {
@@ -38,7 +39,7 @@ class BookRequest(BaseModel):
                 'author': 'surya',
                 'description': 'A new description of a book',
                 'rating': 5,
-                'published_date':2019
+                'published_date': 2019
             }
         }
 
@@ -53,22 +54,21 @@ BOOKS = [
 ]
 
 
-@app.get("/books")
+@app.get("/books", status_code=status.HTTP_200_OK)
 async def read_all_books():
     return BOOKS
 
 
-
-@app.get("/books/{book_id}")
+@app.get("/books/{book_id}", status_code=status.HTTP_200_OK)
 async def read_book_by_id(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Item not found')
 
 
-
-@app.get("/books/")
-async def read_books_by_rating(book_rating: int):
+@app.get("/books/", status_code=status.HTTP_200_OK)
+async def read_books_by_rating(book_rating: int = Query(gt=0, lt=6)):
     list_of_books = []
     for book in BOOKS:
         if book.rating == book_rating:
@@ -76,33 +76,42 @@ async def read_books_by_rating(book_rating: int):
     return list_of_books
 
 
-@app.get("/books/publish/{year}")
-async def read_books_by_published_year(year:int):
+@app.get("/books/publish/{year}", status_code=status.HTTP_200_OK)
+async def read_books_by_published_year(year: int = Path(gt=1999)):
     list_of_books = []
     for book in BOOKS:
         if book.published_date == year:
             list_of_books.append(book)
     return list_of_books
 
-@app.post("/create-book")
+
+@app.post("/create-book", status_code=status.HTTP_201_CREATED)
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.dict())
     BOOKS.append(find_book_id(new_book))
 
 
-@app.put("/books/update_book")
+@app.put("/books/update_book", status_code=status.HTTP_204_NO_CONTENT)
 async def update_book(book: BookRequest):
+    book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book.id:
             BOOKS[i] = book
+            book_changed = True
+    if not book_changed:
+        raise HTTPException(status_code=404, detail='Item not found')
 
 
-@app.delete("/books/{book_id}")
+@app.delete("/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book_by_id(book_id: int = Path(gt=0)):
+    book_changed = False
     for book in BOOKS:
         if book.id == book_id:
             BOOKS.remove(book)
+            book_changed = True
             break
+    if not book_changed:
+        raise HTTPException(status_code=404, detail='Item not found')
 
 
 def find_book_id(book: Book):
