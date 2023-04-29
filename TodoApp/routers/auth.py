f3e1de50c223af9cd61782fabe49516f8d16cf86
git-suryacs <2suryacs@gmail.com>
2023-04-29 12:04:20 +0100

    Decoding the JSON Web Token

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,14 +1,14 @@
 from datetime import timedelta,datetime
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from starlette import status
 from database import SessionLocal
 from models import Users
 from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordRequestForm
-from jose import jwt
+from fastapi.security import OAuth2PasswordRequestForm,OAuth2PasswordBearer
+from jose import jwt, JWTError
 
 router = APIRouter()
 SECRET_KEY = 'c5e5b81722c61bc29ea7f2847c81a11723113ffcfe968b9ae0be5132b05c7bc8'
@@ -16,6 +16,8 @@ ALGORITHM = 'HS256'
 
 
 bcrypt_context = CryptContext(schemes=['bcrypt'],deprecated='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
+
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -51,6 +53,18 @@ def create_access_token(username:str,user_id:int,expires_delta:timedelta):
     expires = datetime.utcnow()+expires_delta
     encode.update({'exp':expires})
     return jwt.encode(encode,SECRET_KEY,algorithm=ALGORITHM)
+
+async def get_current_user(token:Annotated[str,Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Could not validate the credentials")
+        return {'username':username,'id':user_id}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate the credentials")
+
 
 
 
