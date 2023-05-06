e9645fa1571d3af850505d4a6f02c39478d9a872
git-suryacs <2suryacs@gmail.com>
2023-05-06 17:55:28 +0100

    Added admin router

--- TodoApp/routers/users.py
+++ TodoApp/routers/users.py
@@ -40,13 +40,13 @@ async def get_user(user: user_dependency, db: db_dependency):
 
 
 @router.put("/password/", status_code=status.HTTP_204_NO_CONTENT)
-async def change_password(user: user_dependency, db: db_dependency, user_verfication: UserVerification):
+async def change_password(user: user_dependency, db: db_dependency, user_verification: UserVerification):
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Authentication Failed')
     user_model = db.query(Users).filter(Users.id == user.get('id')).first()
 
-    if not bcrypt_context.verify(user_verfication.password, user_model.hashed_password):
+    if not bcrypt_context.verify(user_verification.password, user_model.hashed_password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Error on password change')
-    user_model.hashed_password = bcrypt_context.hash(user_verfication.new_password)
+    user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
     db.add(user_model)
     db.commit()
