35e2cf9d862a6328d809c868485a1c3bf4625070
git-suryacs <2suryacs@gmail.com>
2023-05-07 19:22:26 +0100

    postgresql connection

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -26,7 +26,7 @@ class TodoRequest(BaseModel):
     title: str = Field(min_length=3)
     description: str = Field(min_length=3, max_length=100)
     priority: int = Field(gt=0, lt=6)
-    completed: bool
+    complete: bool
 
 
 @router.get("/", status_code=status.HTTP_200_OK)
@@ -50,6 +50,7 @@ async def read_todo(user: user_dependency, db: db_dependency, todo_id: int = Pat
 async def create_todo(user: user_dependency,
                       db: db_dependency,
                       todo_request: TodoRequest):
+    print("INSIDE")
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication Failed")
     todo_model = Todos(**todo_request.dict(), owner_id=user.get('id'))
@@ -72,8 +73,7 @@ async def update_todo(user: user_dependency, db: db_dependency,
     todo_model.title = todo_request.title
     todo_model.description = todo_request.description
     todo_model.priority = todo_request.priority
-    todo_model.completed = todo_request.completed
-
+    todo_model.complete = todo_request.complete
     db.add(todo_model)
     db.commit()
 
